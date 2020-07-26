a7b36975db6ab4e26d9e2ed05b5dec2ed580df0a
chrisrm86 <christianrmoran86@gmail.com>
2020-07-25 23:22:09 -0300

    Deleted text-alignment:left and added PDF examples

--- Text_to_PDF.py
+++ Text_to_PDF.py
@@ -16,26 +16,23 @@ def convert_to_pdf():
     print("Convert txt to PDF")
     text_filename = input("Text file name: ")
     pdf_filename = input("PDF file name: ")
-    print('1 - Left')
-    print('2 - right')
-    print('3 - center')
-    print('4 - justify')
+    print('1 - Justify')
+    print('2 - Center')
+    print('3 - Right')
     alignment = str(input('Choose Alignment: '))
     if alignment == '1':
-        align = 'L'
-    elif alignment == '2':
-        align = 'R'
-    elif alignment == '3':
-        align = 'C'
-    elif alignment == '4':
         align = 'J'
+    elif alignment == '2':
+        align = 'C'
+    elif alignment == '3':
+        align = 'R'
     else:
         print("Invalid option")
         return convert_to_pdf()
 
     pdf_file = FPDF()
     pdf_file.add_page()
-    pdf_file.set_font("Arial", size=15)
+    pdf_file.set_font("Arial", size=10)
     text_file = open('{}.txt'.format(text_filename), "r")
     for x in text_file:
         pdf_file.cell(200, 10, txt=x, ln=1, align = align)
